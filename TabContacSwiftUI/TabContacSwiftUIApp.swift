2178f2ae78295ad6dcfb46b62a30b83c71771c25
Briniel <lord.devol@gmail.com>
2021-11-12 14:11:19 +0300

    Initial Commit

---
+++ TabContacSwiftUI/TabContacSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabContacSwiftUIApp.swift
+//  TabContacSwiftUI
+//
+//  Created by Михаил Иванов on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TabContacSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
